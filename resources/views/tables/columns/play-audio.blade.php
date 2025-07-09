962bac11e532d0ad04511991c2f2eadea3fb380d
cindinvnt611 <0110124072@student.nurulfikri.ac.id>
2025-07-09 21:05:55 +0700

    project2 fix kelompok 9

--- resources/views/tables/columns/play-audio.blade.php
+++
@@ -1,4 +0,0 @@
-<audio controls style="width: 150px;">
-    <source src="{{ Storage::url($getRecord()->audio_path) }}" type="audio/mpeg">
-    Your browser does not support the audio element.
-</audio>
